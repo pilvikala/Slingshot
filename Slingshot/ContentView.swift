88535dd7cca3758da89a57a34aaa0758a8b6c3d6
pilvikala <michal.brutvan@gmail.com>
2023-08-04 14:15:50 +0300

    Initial Commit

---
+++ Slingshot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Slingshot
+//
+//  Created by Michal Brutvan on 4.8.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slingshot/SlingshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlingshotApp.swift
+//  Slingshot
+//
+//  Created by Michal Brutvan on 4.8.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlingshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
